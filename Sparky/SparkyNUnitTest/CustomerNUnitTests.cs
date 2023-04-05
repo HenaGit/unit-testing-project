db566863a1fffdc73815fd2ee7798cd4ac91b0b9
henokg <heni.gebrehiwot@gmail.com>
2023-04-05 16:28:04 -0600

    Section 4 - Range Assert

--- Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -46,5 +46,11 @@ namespace SparkyNUnitTest
             //assert
             Assert.IsNull(customer.GreetMessage);
         }
+        [Test]
+        public void DiscountCheck_DefaultCustomer_ReturnsDiscountInRange()
+        {
+            int result = customer.Discount;
+            Assert.That(result, Is.InRange(10, 25));
+        }
     }
 }
