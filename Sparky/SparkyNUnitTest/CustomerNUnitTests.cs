c3aacce14e3e20b12f3bad034051d14079c9a5a6
henokg <heni.gebrehiwot@gmail.com>
2023-04-05 15:41:03 -0600

    Section 3 - Global Initialization for Class

--- Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -11,29 +11,35 @@ namespace SparkyNUnitTest
     [TestFixture]
     internal class CustomerNUnitTests
     {
+        private Customer customer;
+        [SetUp]
+        public void Setup()
+        {
+            customer = new Customer();
+        }
         [Test]
         public void CombineName_InputFirstAndLastName_ReturnFullName()
         {
             //Arrange
-            var customer = new Customer();
+            
 
             //Act
-            string fullName = customer.GreetAndCombineNames("Henok", "Gebrehiwot");
+            customer.GreetAndCombineNames("Henok", "Gebrehiwot");
 
             //Assert
-            //Assert.That(fullName, Is.EqualTo("Hello, Henok Gebrehiwot"));
-            Assert.AreEqual(fullName, "Hello, Henok Gebrehiwot");
-            Assert.That(fullName, Is.EqualTo("Hello, Henok Gebrehiwot"));
-            Assert.That(fullName, Does.Contain("Henok Gebrehiwot").IgnoreCase);
-            Assert.That(fullName, Does.StartWith("Hello,"));
-            Assert.That(fullName, Does.EndWith("Gebrehiwot"));
-            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+            //Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Henok Gebrehiwot"));
+            Assert.AreEqual(customer.GreetMessage, "Hello, Henok Gebrehiwot");
+            Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Henok Gebrehiwot"));
+            Assert.That(customer.GreetMessage, Does.Contain("Henok Gebrehiwot").IgnoreCase);
+            Assert.That(customer.GreetMessage, Does.StartWith("Hello,"));
+            Assert.That(customer.GreetMessage, Does.EndWith("Gebrehiwot"));
+            Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
         }
         [Test]
         public void GreetMessage_NotGreeted_ReturnsNull()
         {
             //arrange
-            var customer = new Customer();
+            
 
             //act
 
