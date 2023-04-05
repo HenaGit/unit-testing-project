ae37de4a655519ea8483d038fcf914aa661d9f2f
henokg <heni.gebrehiwot@gmail.com>
2023-04-05 15:20:13 -0600

    Section 3 - Asserting String Values

---
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    internal class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineName_InputFirstAndLastName_ReturnFullName()
+        {
+            //Arrange
+            var customer = new Customer();
+
+            //Act
+            string fullName = customer.GreetAndCombineNames("Henok", "Gebrehiwot");
+
+            //Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Henok Gebrehiwot"));
+        }
+    }
+}
