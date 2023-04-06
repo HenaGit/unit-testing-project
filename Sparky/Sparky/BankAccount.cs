3590e8fc2572aeda8f21b74b07ceb26d0c6b7c4a
henokg <heni.gebrehiwot@gmail.com>
2023-04-06 23:35:45 +0300

    Section 5 - Setup Bank Account Class

---
+++ Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    internal class BankAccount
+    {
+        public int balance { get; set; }
+        public BankAccount()
+        {
+            balance = 0;
+        }
+        public bool Deposit(int amount)
+        {
+            balance += amount;
+            return true;
+        }
+        public bool Withrdraw(int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public int GetBalance() { return balance; }
+    }
+}
