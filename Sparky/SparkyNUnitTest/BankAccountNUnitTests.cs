7f2dfd1a5ac9869d01c5b066e4bb7f654e521043
henokg <heni.gebrehiwot@gmail.com>
2023-04-07 20:56:33 +0300

    Section 5 - MOQ Properties

--- Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
+++ Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -100,5 +100,19 @@ namespace SparkyNUnitTest
             Assert.IsTrue(logMock.Object.LogWithRefObj(ref customer));
 
         }
+        [Test]
+        public void BankLogDummy_SetAndGetLogTypeAndSeveirtyMock_MockTest()
+        {
+            var logMock = new Mock<ILogBook>();
+            
+            logMock.Setup(u => u.LogSeverity).Returns(10);
+            logMock.SetupAllProperties();
+            logMock.Setup(u => u.LogType).Returns("warning");
+            
+
+            logMock.Object.LogSeverity = 100;
+            Assert.That(logMock.Object.LogSeverity, Is.EqualTo(100));
+            Assert.That(logMock.Object.LogType, Is.EqualTo("warning"));
+        }
     }
 }
