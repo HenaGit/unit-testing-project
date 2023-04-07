8fd60f952e0677daf5e95353f21dd3e77d31d943
henokg <heni.gebrehiwot@gmail.com>
2023-04-07 22:22:45 +0300

    Section 6 - Xunit Calculator Part 2

--- Sparky/SparkyXUnit/CalculatorXUnitTests.cs
+++ Sparky/SparkyXUnit/CalculatorXUnitTests.cs
@@ -50,5 +50,40 @@ namespace SparkyXUnit
             var result = calc.IsOddNumber(a);
             Assert.Equal(expectedResult, result);
         }
+        [Theory]
+        [InlineData(5.4, 10.5)] //15.9
+        //[InlineData(5.43, 10.53)] // 15.96
+        //[InlineData(5.49, 10.59)] // 16.08
+        public void AddNumbersDouble_InputTwoDouble_GetCorrectAddition(double a, double b)
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            double result = calc.AddNumbersDouble(a, b);
+
+
+            //Assert
+            Assert.Equal(15.9, result, 1);
+            //15.7-16.1
+        }
+        [Fact]
+        public void OddRanger_InputMinAndMaxRange_ReturnsValidOddNumberRange()
+        {
+            Calculator calc = new();
+            List<int> expectedOddRange = new() { 5, 7, 9 }; //5-10
+
+            //act
+            List<int> result = calc.GetOddRange(5, 10);
+
+            //Assert
+            Assert.Equal(expectedOddRange, result);
+            Assert.Contains(7, result);
+            Assert.NotEmpty(result);
+            Assert.Equal(3, result.Count);
+            Assert.DoesNotContain(6, result);
+            Assert.Equal(result.OrderBy(u => u), result);
+            //Assert.That(result, Is.Unique);
+        }
     }
 }
