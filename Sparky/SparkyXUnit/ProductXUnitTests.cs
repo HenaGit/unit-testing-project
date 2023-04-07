eed0f9adde384c0ffe7c5008db6338282c4fa76c
henokg <heni.gebrehiwot@gmail.com>
2023-04-07 23:01:17 +0300

    Section 6 - MOQ and Xunit

--- Sparky/SparkyXUnit/ProductXUnitTests.cs
+++ Sparky/SparkyXUnit/ProductXUnitTests.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Moq;
+using Sparky;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SparkyXUnit
 {
-    internal class ProductXUnitTests
+    public class ProductXUnitTests
     {
+        [Fact]
+        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            Product product = new() { Price = 50 };
+
+            var result = product.GetPrice(new Customer() { IsPlatinum = true });
+
+            Assert.Equal(40, result);
+        }
+        [Fact]
+        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(u => u.IsPlatinum).Returns(true);
+
+            Product product = new Product() { Price = 50 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.Equal(40, result);
+        }
     }
 }
