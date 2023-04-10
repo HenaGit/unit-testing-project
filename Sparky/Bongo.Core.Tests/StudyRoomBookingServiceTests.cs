c775db3a69b7a2a2e20a584531afa05693e2975d
henokg <heni.gebrehiwot@gmail.com>
2023-04-10 22:12:54 +0300

    Section 7 - Bongo Core MOQ Setup and GetAllBooking Test

---
+++ Sparky/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Core.Tests
+{
+    [TestFixture]
+    public class StudyRoomBookingServiceTests
+    {
+        private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepoMock;
+        private Mock<IStudyRoomRepository> _studyRoomRepoMock;
+        private StudyRoomBookingService _bookingService;
+        [SetUp]
+        public void Setup()
+        {
+            _studyRoomBookingRepoMock = new Mock<IStudyRoomBookingRepository>();
+            _studyRoomRepoMock = new Mock<IStudyRoomRepository>();
+            _bookingService = new StudyRoomBookingService(
+                _studyRoomBookingRepoMock.Object,
+                _studyRoomRepoMock.Object
+                );
+
+        }
+        [TestCase]
+        public void GetAllBooking_InvokeMethod_CheckIfRepoIsCalled()
+        {
+            _bookingService.GetAllBooking();
+            _studyRoomBookingRepoMock.Verify(x => x.GetAll(null), Times.Once);
+        }
+    }
+}
